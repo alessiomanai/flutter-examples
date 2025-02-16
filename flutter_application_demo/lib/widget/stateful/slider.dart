1eef60bc4b6fadda25c22f2947b22fa2f31b8929
Alessio Manai <alessio.manai@gmail.com>
2025-02-16 14:19:49 +0100

    first commit

---
+++ flutter_application_demo/lib/widget/stateful/slider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SliderExample extends StatefulWidget {
+  const SliderExample({super.key});
+
+  @override
+  State<SliderExample> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<SliderExample> {
+  double _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Slider')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+                    
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+          Slider(
+            max: 10,
+            divisions: 10,
+            value: _counter,
+            label: _counter.round().toInt().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _counter = value;
+              });
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
